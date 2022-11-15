f8c292f7216a564eb1b34b874036da3c8ec7bdb5
jakub <jakub.tarnacki1@gmail.com>
2022-11-15 18:02:12 +0100

    zad1

---
+++ zad1/src/Human.java
@@ -0,0 +1,11 @@
+public class Human
+{
+Animal pet;
+String firstName;
+String lastName;
+final Integer yearOfBirth;
+Human(Integer yearOfBirth)
+{
+    this.yearOfBirth = yearOfBirth;
+}
+}
---
+++ zad1/src/Main.java
@@ -0,0 +1,29 @@
+public class Main
+{ public static void main(String[] args)
+{
+        Animal dog = new Animal("canis");
+        Human me = new Human(2001);
+        me.pet = dog;
+
+            dog.feed();
+            dog.walk();
+            dog.walk();
+            dog.walk();
+            dog.walk();
+            dog.walk();
+            dog.walk();
+            dog.walk();
+            dog.walk();
+            dog.walk();
+            dog.walk();
+            dog.walk();
+            dog.walk();
+            dog.walk();
+            dog.walk();
+            dog.walk();
+            dog.walk();
+            dog.walk();
+            dog.walk();
+            dog.feed();
+    }
+}
