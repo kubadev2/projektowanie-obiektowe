545afd37fc981ca6de9039bbb42fa973e8cab636
jakub <jakub.tarnacki1@gmail.com>
2022-11-15 20:03:53 +0100

    zad2

---
+++ zad2/src/Human.java
@@ -0,0 +1,12 @@
+public class Human
+{
+    Animal pet;
+    String firstName;
+    String lastName;
+    Car car;
+    final Integer yearOfBirth;
+    Human(Integer yearOfBirth)
+    {
+        this.yearOfBirth = yearOfBirth;
+    }
+}
